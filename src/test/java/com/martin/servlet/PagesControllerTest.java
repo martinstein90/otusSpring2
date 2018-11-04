f3636a97eeeec03536789a917d6be6383caff953
martin <martimstein90@yahoo.com>
2018-11-04 18:53:11 +0300

    Готово!!

--- src/test/java/com/martin/servlet/PagesControllerTest.java
+++ src/test/java/com/martin/servlet/PagesControllerTest.java
@@ -2,11 +2,13 @@ package com.martin.servlet;
 
 
 import com.martin.security.AuthenticationManagerImpl;
+import com.martin.security.SecurityConfiguration;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
 import org.springframework.security.test.context.support.WithAnonymousUser;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -18,6 +20,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(PagesController.class)
+@Import(SecurityConfiguration.class)
 public class PagesControllerTest {
 
     @Autowired
@@ -26,12 +29,21 @@ public class PagesControllerTest {
     @MockBean
     private AuthenticationManagerImpl authenticationManagerImpl;
 
-
-    //@WithAnonymousUser()
-    @WithMockUser(username = "Martin", authorities = {"ADMIN"})
+    @WithAnonymousUser
     @Test
     public void testPublic() throws Exception {
-        mockMvc.perform(get("/public")).andExpect(status().isOk());
+        mockMvc.perform(get("/")).andExpect(status().isOk());
     }
 
+    @WithAnonymousUser
+    @Test
+    public void testRedirectUnauthorized() throws Exception {
+        mockMvc.perform(get("/authenticated")).andExpect(status().is3xxRedirection());
+    }
+
+    @WithMockUser
+    @Test
+    public void testAccessAuthorized() throws Exception {
+        mockMvc.perform(get("/authenticated")).andExpect(status().isOk());
+    }
 }
