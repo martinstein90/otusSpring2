c4ca12d397f356c8eafee80bb79c08f06402f055
martin <martimstein90@yahoo.com>
2018-11-21 10:27:59 +0300

    готово

--- src/main/java/com/martin/rest/MoscowCurrentTimeRestController.java
+++ src/main/java/com/martin/rest/MoscowCurrentTimeRestController.java
@@ -2,11 +2,13 @@ package com.martin.rest;
 
 import com.martin.domain.CurrentTimeDto;
 import com.martin.services.CurrentTimeService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 public class MoscowCurrentTimeRestController {
 
     @Autowired
@@ -14,7 +16,7 @@ public class MoscowCurrentTimeRestController {
 
     @GetMapping("/time")
     public CurrentTimeDto getCurrentTime() {
-        System.out.println("getCurrentTime");
+        log.info("getCurrentTime");
         return moscowCurrentTimeService.getCurrentTimeFromYandex();
     }
 }
--- src/main/java/com/martin/services/MoscowCurrentTimeServiceImpl.java
+++ src/main/java/com/martin/services/MoscowCurrentTimeServiceImpl.java
@@ -2,14 +2,15 @@ package com.martin.services;
 
 import com.martin.domain.CurrentTimeDto;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-
 @Service
+@Slf4j
 public class MoscowCurrentTimeServiceImpl implements CurrentTimeService {
 
     private final RestTemplate template;
@@ -23,11 +24,13 @@ public class MoscowCurrentTimeServiceImpl implements CurrentTimeService {
                     commandKey = "getCurrentTimeFromYandex",
                     fallbackMethod = "getCurrentTimeFromServer")
     public CurrentTimeDto getCurrentTimeFromYandex() {
-        return template.getForObject("https://yandex.com/time/sync.json?geo=213", CurrentTimeDto.class);
+        log.info("From yandex");
+        return template.getForObject("https://yandex1.com/time/sync.json?geo=213", CurrentTimeDto.class);
     }
 
     @Override
     public CurrentTimeDto getCurrentTimeFromServer() {
+        log.info("From localhost");
         CurrentTimeDto currentTimeDto = new CurrentTimeDto();
         SimpleDateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy HH:mm:ss");
         currentTimeDto.setCurrentTime(dateFormat.format(new Date()));
